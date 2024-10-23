565061c0db3b4d15a784838f09592128947dda03
Adolky <amakency@gmail.com>
2024-10-23 16:45:32 -0400

    Update

--- src/Oct/Ex_10032024_Main_Method/Lab003.java
+++ src/Oct/Ex_10032024_Main_Method/Lab003.java
@@ -2,9 +2,9 @@ package Oct.Ex_10032024_Main_Method;
 
 public class Lab003 {
     public static void main (String[] args) {
-        System.out.println("10+2 = 12");
-        System.out.println("10-2 = 8");
-        System.out.println("10*2 = 20");
-        System.out.println("10/2 = 5");
+        System.out.println("10 + 2 = 12");
+        System.out.println("10 - 2 = 8");
+        System.out.println("10 * 2 = 20");
+        System.out.println("10 / 2 = 5");
     }
 }
