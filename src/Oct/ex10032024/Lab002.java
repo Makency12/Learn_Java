5f5950e3c11b34b6462b2c62c9fae1b8fda71936
Adolky <amakency@gmail.com>
2024-10-23 16:11:27 -0400

    Update

--- src/Oct/ex10032024/Lab002.java
+++ src/Oct/ex10032024/Lab002.java
@@ -1,4 +1,17 @@
 package Oct.ex10032024;
 
 public class Lab002 {
+    public static void main (String[] args) {
+      System.out.println("10+1 = 11");
+      System.out.println("10+2 = 12");
+      System.out.println("10+3 = 13");
+      System.out.println("10+4 = 14");
+      System.out.println("10+5 = 15");
+      System.out.println("10+6 = 16");
+      System.out.println("10+7 = 17");
+      System.out.println("10+8 = 18");
+      System.out.println("10+9 = 19");
+      System.out.println("10+10 = 20");
+    }
 }
+
