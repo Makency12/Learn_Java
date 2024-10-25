8465e82d69cc521881ae861276ee0289dbe7413f
Adolky <amakency@gmail.com>
2024-10-25 11:29:21 -0400

    Update

---
+++ src/Oct/Ex_10092024_Data_Types/Lab026.java
@@ -0,0 +1,31 @@
+package Oct.Ex_10092024_Data_Types;
+
+public class Lab026 {
+
+    public static void main (String[] args) {
+     int table = 15;
+     System.out.printf("%d x %d = %d", table, 1, table*1);
+     System.out.println();
+     System.out.printf("%d x %d = %d", table, 2, table*2);
+     System.out.println();
+     System.out.printf("%d x %d = %d", table, 3, table*3);
+     System.out.println();
+     System.out.printf("%d x %d = %d", table, 4, table*4);
+     System.out.println();
+     System.out.printf("%d x %d = %d", table, 5, table*5);
+     System.out.println();
+     System.out.printf("%d x %d = %d", table, 6, table*6);
+     System.out.println();
+     System.out.printf("%d x %d = %d", table, 7, table*7);
+     System.out.println();
+     System.out.printf("%d x %d = %d", table, 8, table*8);
+     System.out.println();
+     System.out.printf("%d x %d = %d", table, 9, table*9);
+     System.out.println();
+     System.out.printf("%d x %d = %d", table, 10, table*10);
+     System.out.println();
+     System.out.printf("%d x %d = %d", table, 11, table*11);
+     System.out.println();
+     System.out.printf("%d x %d = %d", table, 12, table*12);
+    }
+}
